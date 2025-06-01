4aa547f0eafb29c792c4f2f98a9c51d9ed7c91e8
Anze3PR <anze.pecarr@gmail.com>
2025-06-01 20:36:32 +0200

    zapletini testira

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-
+##test za update kode, ker neznam drugače testirati lp
 @app.get("/")
 async def read_root():
     return {"message": "Hello, World!"}
+
+def test():
+    print("zaplet")
