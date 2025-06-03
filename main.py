6c61fda093e111b484179547e1e9ae9e8ade6ad5
Anze3PR <anze.pecarr@gmail.com>
2025-06-03 13:51:52 +0200

    dodan get_item in inventory

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-##test za update kode, ker neznam drugače testirati lp
+
+item_db = [{"item_name": "Spaget", "item_qty":15, "location": "Spajza"} , {"item_name":"Mleko", "item_qty": 5, "location":"Hladilnik"}]
+
 @app.get("/")
 async def read_root():
     return {"message": "Hello, World!"}
 
-def test():
-    print("zaplet")
+@app.get("/item/{item_name}")
+async def item_details(item_name):
+    for item in item_db:
+        if item["item_name"] == item_name:
+         return {"item_name":item["item_name"],
+            "item_qty":item["item_qty"]}
+    return("item not found")
+
+@app.get("/inventory")
+async def get_inventory(location: str = None):
+    if location == None:
+        return item_db
+    else:
+        items = []
+        for item in item_db:
+            if item.get("location") == location:
+                items.append(item)
+        return items
+
+
