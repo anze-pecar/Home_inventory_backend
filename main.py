87a03f1401d609d9f3aec2c600264ad59435f7eb
Anze3PR <anze.pecarr@gmail.com>
2025-06-03 15:02:27 +0200

    Nov branch za DB

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException, Query
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from typing import Annotated
+
+class Item(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True, unique_items=True)
+    name: str = Field(index=True)
+    parent_item_id: None = Field(default=None, foreign_key=True)
+    secret_name: str
+
+sqllite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqllite_file_name}"
+
+connect_args = {"check_same_thread" : False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+def get_session():
+    with Session(engine) as sesstion:
+        yield sesstion
+SessionDep = Annotated[Session, Depends(get_session)]
 
 app = FastAPI()
 
-item_db = [{"item_name": "Spaget", "item_qty":15, "location": "Spajza"} , {"item_name":"Mleko", "item_qty": 5, "location":"Hladilnik"}]
-
-@app.get("/")
-async def read_root():
-    return {"message": "Hello, World!"}
-
-@app.get("/item/{item_name}")
-async def item_details(item_name):
-    for item in item_db:
-        if item["item_name"] == item_name:
-         return {"item_name":item["item_name"],
-            "item_qty":item["item_qty"]}
-    return("item not found")
-
-@app.get("/inventory")
-async def get_inventory(location: str = None):
-    if location == None:
-        return item_db
-    else:
-        items = []
-        for item in item_db:
-            if item.get("location") == location:
-                items.append(item)
-        return items
-
+@app.lifespan("startup")
+def on_startup():
+    create_db_and_tables()
+@app.post("/item/")
+def add_item(item: Item, session: SessionDep) -> Item:
+    session.add(item)
+    session.commit()
+    session.refresh(item)
+    return item
+@app.get("/items/")
+def read_items(
+    session: SessionDep)-> list[Item]:
+    items = session.exec(select(Item).all())
+    return items
 
